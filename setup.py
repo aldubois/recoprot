0aa54f8546f6685875d04e9cd36baa53af8c217b
Alexandre Dubois <alexandre.dubois@colostate.edu>
2021-08-12 10:50:47 -0600

    [Start] Repository structure and test data

---
+++ setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='recoprot',
+    version='0.1.0',
+    description='Package to run ML algorithms for protein recognition',
+    long_description=readme,
+    author='Alexandre Dubois',
+    author_email='alexandre.dubois@colostate.edu',
+    url='https://github.com/aldubois/recoprot',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'env'))
+)
