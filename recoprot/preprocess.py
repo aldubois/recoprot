66c72e66e94c8e4e24f2c749be1f90b0a9612f0d
Alexandre Dubois <alexandre.dubois@colostate.edu>
2021-08-22 23:28:12 -0600

    [Dev] Encoding of the residues list function.

--- recoprot/preprocess.py
+++ recoprot/preprocess.py
@@ -8,8 +8,37 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 
 
-CATEGORIES = ['1', 'C', 'CA', 'CB', 'CG', 'CH2', 'N',
-              'NH2', 'O1', 'O2', 'OG','OH', 'SE']
+CATEGORIES = {
+    "atoms": ['1', 'C', 'CA', 'CB', 'CG', 'CH2', 'N',
+              'NH2', 'O1', 'O2', 'OG','OH', 'SE'],
+    "residues": ['1', 'ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN',
+                 'GLU', 'GLY', 'HIS', 'ILE', 'LEU', 'LYS', 'MET',
+                 'PHE', 'PRO', 'SER', 'THR', 'TRP', 'TYR', 'VAL']
+}
+
+RESIDUES_TABLE = {
+    "ALA": "A",
+    "CYS": "C",
+    "ASP": "D",
+    "GLU": "E",
+    "PHE": "F",
+    "GLY": "G",
+    "HIS": "H",
+    "ILE": "I",
+    "LYS": "K",
+    "LEU": "L",
+    "MET": "M",
+    "ASN": "N",
+    "PRO": "P",
+    "GLN": "Q",
+    "ARG": "R",
+    "SER": "S",
+    "THR": "T",
+    "VAL": "V",
+    "TRP": "W",
+    "TYR": "Y"
+}
+
 
 def pdb2fasta(chain):
     """
@@ -26,29 +55,7 @@ def pdb2fasta(chain):
         FASTA sequence.
     """
     # Table extracted from https://cupnet.net/pdb2fasta/
-    TABLE = {
-        "ALA": "A",
-        "CYS": "C",
-        "ASP": "D",
-        "GLU": "E",
-        "PHE": "F",
-        "GLY": "G",
-        "HIS": "H",
-        "ILE": "I",
-        "LYS": "K",
-        "LEU": "L",
-        "MET": "M",
-        "ASN": "N",
-        "PRO": "P",
-        "GLN": "Q",
-        "ARG": "R",
-        "SER": "S",
-        "THR": "T",
-        "VAL": "V",
-        "TRP": "W",
-        "TYR": "Y"
-    }
-    return "".join(TABLE[residu.get_resname()]
+    return "".join(RESIDUES_TABLE[residu.get_resname()]
                    for residu in chain.get_residues())
 
 
@@ -67,9 +74,32 @@ def encode_protein_atoms(atoms):
         Encoded atoms chain in a Compressed Sparse Row format.
     """
     encoder = OneHotEncoder(handle_unknown='ignore')
-    categories = np.array(CATEGORIES).reshape(-1, 1)
+    categories = np.array(CATEGORIES["atoms"]).reshape(-1, 1)
     encoder.fit(categories)
     categorized_atoms = np.array([atom if atom in categories else '1'
                                   for atom in atoms])
     encoded_atoms = encoder.transform(categorized_atoms.reshape(-1, 1))
     return encoded_atoms
+
+
+def encode_protein_residues(residues):
+    """
+    Encode protein residues list into integer array.
+
+    Parameters
+    ----------
+    residues: list of str
+        List of residues name from a protein.
+
+    Returns
+    -------
+    {ndarray, sparse matrix} of shape (n_residues, n_encoded_features)
+        Encoded atoms chain in a Compressed Sparse Row format.
+    """
+    encoder = OneHotEncoder(handle_unknown='ignore')
+    categories = np.array(CATEGORIES["residues"]).reshape(-1, 1)
+    encoder.fit(categories)
+    categorized_residues = np.array([residue if residue in categories else '1'
+                                     for residue in residues])
+    encoded_residues = encoder.transform(categorized_residues.reshape(-1, 1))
+    return encoded_residues
--- tests/preprocess_test.py
+++ tests/preprocess_test.py
@@ -25,7 +25,9 @@ def test_pdb2fasta():
 
 
 def test_encode_protein_atoms():
-    
+    """
+    Test the encoding of a list of atoms compared to a manually compared one.
+    """    
     atoms = ['O2', 'CB', "H", "N", "SE", "O", "CG"]
     ref = np.array(
         [[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -37,6 +39,25 @@ def test_encode_protein_atoms():
          [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]]
     )
     calc = recoprot.encode_protein_atoms(atoms)
+    assert (calc.toarray() == ref).all()
+    return
+
+
+def test_encode_protein_residues():
+    """
+    Test the encoding of a list of residues compared to a manually compared one.
+    """
+    residues = ['ILE', 'TRP', "ARG", "ZZZ", "GLN", "PHE", "ASN"]
+    ref = np.array(
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    )
+    calc = recoprot.encode_protein_residues(residues)
     print(calc.toarray())
     print(ref)
     assert (calc.toarray() == ref).all()
